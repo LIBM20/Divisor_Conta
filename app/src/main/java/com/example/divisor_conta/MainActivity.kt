6bf67b08ec5a815dc18196b2409abb97a31e6a60
larab <libm20@outlook.com>
2024-06-11 10:01:30 +0100

    Functions refactor and adding of scaffold to the theme in MainActivity commit

--- app/src/main/java/com/example/divisor_conta/MainActivity.kt
+++ app/src/main/java/com/example/divisor_conta/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.divisor_conta.ui.theme.Divisor_ContaTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Divisor_ContaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 48.dp, bottom = 48.dp)) { innerPadding -> DivisorLayout(modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize())
                 }
             }
         }
@@ -30,17 +32,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DivisorLayout(modifier: Modifier = Modifier) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DivisorPreview() {
     Divisor_ContaTheme {
-        Greeting("Android")
+        DivisorLayout()
     }
 }
