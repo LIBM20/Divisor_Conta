de0c8e97db65c278f424afa6fecbc1c15c370cf4
larab <libm20@outlook.com>
2024-06-24 02:54:04 +0100

    Individual tips are now in another page, colors were changed Commit

--- app/src/main/java/com/example/divisor_conta/MainActivity.kt
+++ app/src/main/java/com/example/divisor_conta/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,6 +22,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
@@ -36,15 +39,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.divisor_conta.ui.theme.Divisor_ContaTheme
 import java.text.NumberFormat
+import java.util.Currency
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,10 +62,10 @@ class MainActivity : ComponentActivity() {
             Divisor_ContaTheme {
                 // A surface container using the 'background' color from the theme
                 Scaffold(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 16.dp, bottom = 16.dp)) { innerPadding -> DivisorLayout(modifier = Modifier
+                    .fillMaxSize()) { innerPadding -> DivisorLayout(modifier = Modifier
                     .padding(innerPadding)
                     .fillMaxSize())
+                    //.padding(top = 16.dp, bottom = 16.dp)
                 }
             }
         }
@@ -84,9 +93,10 @@ fun ButtonImage(
                 )
             }
             Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Dividor de Contas", style  = MaterialTheme.typography.displaySmall)
+            Text(text = "Divisor de Contas", style  = MaterialTheme.typography.displaySmall)
         }
 }
+
 
 @Composable
 fun FieldText(labelText : String, value: String,
@@ -101,11 +111,11 @@ fun FieldText(labelText : String, value: String,
         keyboardOptions = KeyboardOptions.Default.copy(
             keyboardType = KeyboardType.Number,
             imeAction = action),
-        colors = TextFieldDefaults.colors(unfocusedContainerColor = colorResource(id = R.color.teal_light), unfocusedLabelColor = colorResource(
+        colors = TextFieldDefaults.colors(unfocusedContainerColor = colorResource(id = R.color.teal_dark), unfocusedLabelColor = colorResource(
             id = R.color.black
         ), unfocusedIndicatorColor = colorResource(
             id = R.color.teal_dark), focusedContainerColor = colorResource(
-            id = R.color.grey_dark), focusedLabelColor = colorResource(id = R.color.white), focusedIndicatorColor = colorResource(id = R.color.grey_light), focusedTextColor = colorResource(
+            id = R.color.grey_dark), focusedLabelColor = colorResource(id = R.color.white), focusedIndicatorColor = colorResource(id = R.color.teal_light), focusedTextColor = colorResource(
             id = R.color.white
         )),
         modifier = modifier.padding(top = 16.dp, bottom = 16.dp)
@@ -129,11 +139,11 @@ fun GorjetaSwitch(texto: String, gorjeta: Boolean,
             checked = gorjeta,
             onCheckedChange = onGorjetaChanged,
             colors = SwitchDefaults.colors(
-                checkedThumbColor = colorResource(R.color.teal_light),
+                checkedThumbColor = colorResource(R.color.teal_dark),
                 checkedTrackColor = colorResource(R.color.grey_dark),
                 uncheckedThumbColor = colorResource(R.color.grey_dark),
-                uncheckedTrackColor = colorResource(R.color.teal_light),
-                uncheckedBorderColor = colorResource(id = R.color.teal_dark),
+                uncheckedTrackColor = colorResource(R.color.teal_dark),
+                uncheckedBorderColor = colorResource(id = R.color.teal_light),
                 checkedBorderColor = colorResource(id = R.color.grey_dark)
             )
         )
@@ -144,24 +154,29 @@ fun GorjetaSwitch(texto: String, gorjeta: Boolean,
 fun calcConta(pessoas: Int, conta: Double, percentGorjeta: Double): String {
     val gorjeta = percentGorjeta / 100 * conta
     val total = (conta + gorjeta)/pessoas
-    return NumberFormat.getCurrencyInstance().format(total)
+
+    // Formato de moeda para Euro (€) usando o Locale para Português
+    val euroFormat = NumberFormat.getCurrencyInstance(Locale("pt"))
+    euroFormat.currency = Currency.getInstance("EUR")
+
+    return euroFormat.format(total)
 }
 
 @Composable
 fun DivisorLayout(modifier: Modifier = Modifier) {
-    //TextFields iniciais
+    // TextFields iniciais
     var quantInput by remember { mutableStateOf("") }
     var contaInput by remember { mutableStateOf("") }
 
-    //Conversão dos inputs nos textfields
+    // Conversão dos inputs nos textfields
     val quant = quantInput.toIntOrNull() ?: 1
     val conta = contaInput.toDoubleOrNull() ?: 0.0
 
-    //Switces
+    // Switches
     var gorjeta by remember { mutableStateOf(false) }
     var gIndividual by remember { mutableStateOf(false) }
 
-    //Lista para guardar as gorjetas individuais
+    // Lista para guardar as gorjetas individuais
     val percentGorjetaInput = remember { mutableStateListOf<String>() }
     var percentGorjeta = 0.0
 
@@ -169,15 +184,14 @@ fun DivisorLayout(modifier: Modifier = Modifier) {
 
     var currentStep by remember { mutableIntStateOf(1) }
 
-    //Caso não seja inserida um valor de conta o switch de gorjeta não funcionará
-    //Como a qunatidade de pessoas tem como valor default 1, não haverá problema se o utilizador não inserir nenhum valor
-    if(conta == 0.0){
+    // Caso não seja inserida um valor de conta o switch de gorjeta não funcionará
+    // Como a quantidade de pessoas tem como valor default 1, não haverá problema se o utilizador não inserir nenhum valor
+    if (conta == 0.0) {
         gorjeta = false
     }
 
-
-    when(currentStep){
-        1->{
+    when (currentStep) {
+        1 -> {
             ButtonImage(
                 drawableResourceId = R.drawable._011863,
                 contentDescription = "Calculadora",
@@ -186,97 +200,152 @@ fun DivisorLayout(modifier: Modifier = Modifier) {
                 }
             )
         }
-        2->{
+
+        2 -> {
+            //De maneira a poder ter o divisor em baixo foi criada uma box para que este
+            //fique fixo
+            Box(
+                modifier = modifier
+                    .statusBarsPadding()
+                    .fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Divider(
+                        color = colorResource(id = R.color.grey_dark),
+                        thickness = 120.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Column(
+                        modifier = modifier
+                            .weight(1f)
+                            .statusBarsPadding()
+                            .background(colorResource(id = R.color.teal_light)),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        FieldText(
+                            labelText = stringResource(id = R.string.quantidade_de_pessoas),
+                            value = quantInput,
+                            onValueChange = { newValue -> quantInput = newValue },
+                            action = ImeAction.Next
+                        )
+                        FieldText(
+                            labelText = stringResource(id = R.string.conta),
+                            value = contaInput,
+                            onValueChange = { newValue -> contaInput = newValue },
+                            action = ImeAction.Done
+                        )
+                        GorjetaSwitch(
+                            texto = stringResource(id = R.string.pretende_dar_gorjeta),
+                            gorjeta = gorjeta,
+                            onGorjetaChanged = { newValue -> gorjeta = newValue },
+                            modifier = Modifier
+                        )
+                        if (gorjeta) {
+                            // Caso a quantidade de pessoas seja 1, o switch de gorjeta individual não aparecerá
+                            if (quant > 1) {
+                                GorjetaSwitch(
+                                    texto = stringResource(R.string.gorjeta_individual),
+                                    gorjeta = gIndividual,
+                                    onGorjetaChanged = { gIndividual = it },
+                                    modifier = Modifier
+                                )
+                            }
+                            if (!gIndividual) {
+                                FieldText(
+                                    labelText = stringResource(R.string.percentagem_de_gorjeta),
+                                    value = percentGorjetaInput.getOrNull(0) ?: "",
+                                    onValueChange = { newValue ->
+                                        if (percentGorjetaInput.isEmpty()) {
+                                            percentGorjetaInput.add(newValue)
+                                        } else {
+                                            percentGorjetaInput[0] = newValue
+                                        }
+                                    },
+                                    action = ImeAction.Done
+                                )
+                                // Spacer(modifier = Modifier.height(16.dp))
+                                if (percentGorjetaInput.isNotEmpty()) {
+                                    percentGorjeta = percentGorjetaInput[0].toDoubleOrNull() ?: 0.0
+                                }
+                            } else {
+                                currentStep = 3
+                            }
+                        }
+                        else {
+                            percentGorjetaInput.clear()
+                            gIndividual = false
+                        }
+                        // Como os totais para cada pessoa vão aparecer, não vale a pena o total geral aparecer
+                        if (!gIndividual) {
+                            Spacer(modifier = Modifier.height(16.dp))
+                            total = calcConta(
+                                pessoas = quant,
+                                conta = conta,
+                                percentGorjeta = percentGorjeta
+                            )
+                            Text(text = stringResource(R.string.total_geral, total), style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+                    Divider(
+                        color = colorResource(id = R.color.grey_dark),
+                        thickness = 120.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+        3 ->{
             Column(
                 modifier = modifier
                     .statusBarsPadding()
-                    .padding(horizontal = 16.dp)
                     .verticalScroll(rememberScrollState())
-                    .safeDrawingPadding(),
+                    .safeDrawingPadding()
+                    .background(colorResource(id = R.color.teal)),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                FieldText(
-                    labelText = stringResource(id = R.string.quantidade_de_pessoas),
-                    value = quantInput,
-                    onValueChange = { newValue -> quantInput = newValue },
-                    action = ImeAction.Next
+                Text(
+                    text = "Gorjeta Individual",
+                    style = TextStyle(fontSize = 30.sp, color = Color.Black), modifier = Modifier.padding(top = 50.dp, bottom = 20.dp)
                 )
-                FieldText(
-                    labelText = stringResource(id = R.string.conta),
-                    value = contaInput,
-                    onValueChange = { newValue -> contaInput = newValue },
-                    action = ImeAction.Done
-                )
-                GorjetaSwitch(
-                    texto = stringResource(id = R.string.pretende_dar_gorjeta),
-                    gorjeta = gorjeta,
-                    onGorjetaChanged = { newValue -> gorjeta = newValue },
-                    modifier = Modifier
-                )
-                if (gorjeta) {
-                    //Caso a quantidade de pessoas seja 1, o switch de gorjeta individual não aparecerá
-                    if(quant > 1) {
-                        GorjetaSwitch(
-                            texto = stringResource(R.string.gorjeta_individual),
-                            gorjeta = gIndividual,
-                            onGorjetaChanged = { gIndividual = it },
-                            modifier = Modifier.padding(end = 26.dp)
-                        )
-                    }
-                    if(!gIndividual) {
-                        FieldText(
-                            labelText = stringResource(R.string.percentagem_de_gorjeta),
-                            value = percentGorjetaInput.getOrNull(0) ?: "",
-                            onValueChange = { newValue ->
-                                if (percentGorjetaInput.isEmpty()) {
-                                    percentGorjetaInput.add(newValue)
-                                } else {
-                                    percentGorjetaInput[0] = newValue
-                                }
-                            },
-                            action = ImeAction.Done
-                        )
-                        //Spacer(modifier = Modifier.height(16.dp))
-                        if (percentGorjetaInput.isNotEmpty()) {
-                            percentGorjeta = percentGorjetaInput[0].toDoubleOrNull() ?: 0.0
-                        }
-                    }else {
-                        while (percentGorjetaInput.size < quant) {
-                            percentGorjetaInput.add("")
-                        }
-                        for (i in 0 until quant) {
-                            Spacer(modifier = Modifier.height(16.dp))
-                            FieldText(
-                                labelText = stringResource(
-                                    R.string.percentagem_de_gorjeta,
-                                ),
-                                value = percentGorjetaInput[i],
-                                onValueChange = { newValue -> percentGorjetaInput[i] = newValue },
-                                action = ImeAction.Done
-                            )
-                            percentGorjeta = percentGorjetaInput[i].toDoubleOrNull() ?: 0.0
-                            total = calcConta(1, conta / quant, percentGorjeta)
-                            Text(text = stringResource(R.string.total_indivual, i + 1, total))
-                        }
-                    }
-                } else {
-                    percentGorjetaInput.clear()
-                    gIndividual = false
+                while (percentGorjetaInput.size < quant) {
+                    percentGorjetaInput.add("")
                 }
-                //Como os totais para cada pessoa vão aparecer, não vale a pena o total geral aparecer
-                if(!gIndividual) {
-                    Spacer(modifier = Modifier.height(16.dp))
-                    total =
-                        calcConta(pessoas = quant, conta = conta, percentGorjeta = percentGorjeta)
-                    Text(text = stringResource(R.string.total_geral, total))
+                for (i in 0 until quant) {
+                    Spacer(modifier = Modifier.height(10.dp))
+                    FieldText(
+                        labelText = stringResource(R.string.percentagem_de_gorjeta),
+                        value = percentGorjetaInput[i],
+                        onValueChange = { newValue ->
+                            percentGorjetaInput[i] = newValue
+                        },
+                        action = ImeAction.Done
+                    )
+                    percentGorjeta = percentGorjetaInput[i].toDoubleOrNull() ?: 0.0
+                    total = calcConta(1, conta / quant, percentGorjeta)
+                    Text(
+                        text = stringResource(
+                            R.string.total_indivual,
+                            i + 1,
+                            total
+                        ), style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 15.sp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    onClick = { currentStep = 2 },
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.white)),
+                    modifier = Modifier.wrapContentWidth(Alignment.Start)
+                ) {
+                    Text(text = "<-", style = TextStyle(color = Color.Black))
                 }
             }
         }
     }
-
-
-
 }
 
 @Preview(showBackground = true)
